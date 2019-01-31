f7d8c1c22234466c448ecc257dbe542cfa35e4f6
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-01-31 23:19:45 +0800

    feat: 新建数据对象

---
+++ backend/src/main/java/com/nju/sesta/sams/entity/Role.java
@@ -0,0 +1,49 @@
+package com.nju.sesta.sams.entity;
+
+import com.nju.sesta.sams.enums.RoleName;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tbl_role")
+public class Role {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name")
+    private RoleName roleName;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<User>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}

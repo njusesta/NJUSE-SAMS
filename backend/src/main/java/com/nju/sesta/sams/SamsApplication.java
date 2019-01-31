a62cdd1a5238eb38a163d16382d86886510fa4b8
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-01-31 11:12:09 +0800

    init backend project

---
+++ backend/src/main/java/com/nju/sesta/sams/SamsApplication.java
@@ -0,0 +1,14 @@
+package com.nju.sesta.sams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamsApplication.class, args);
+    }
+
+}
+
